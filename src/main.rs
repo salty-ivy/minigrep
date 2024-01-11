200adad920b77cea7d977a77bb7153a89ae94718
Aman Pandey <aman2001mi@gmail.com>
2024-01-12 03:36:09 +0530

    minigrep initial implementaiton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::env;
+use std::fs;
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+
+impl Config {
+    fn new(args: &Vec<String>)-> Config{
+        if args.len() < 3 {
+            panic!(" Not enough arguments");
+        }
+        let query: String = args[1].clone();
+        let file_path: String = args[2].clone();
+    
+        Config{ query, file_path }
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();  // args() call returns an iterator and .collect() will convert it into collection
+    
+    let config: Config = Config::new(&args);
+    
+    println!("Serching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents: String = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    println!("with text:\n\n{contents}");
 }
