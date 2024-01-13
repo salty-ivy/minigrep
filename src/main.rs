656a3b4c4d22ee1918fb2baf7b9d41b7eac6d11a
Aman Pandey <aman2001mi@gmail.com>
2024-01-13 14:24:48 +0530

    complete tests and stderr

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,20 @@
 use std::env;
-use std::fs;
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-
-impl Config {
-    fn new(args: &Vec<String>)-> Config{
-        if args.len() < 3 {
-            panic!(" Not enough arguments");
-        }
-        let query: String = args[1].clone();
-        let file_path: String = args[2].clone();
-    
-        Config{ query, file_path }
-    }
-}
+use std::process;
+use minigrep::Config;
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();  // args() call returns an iterator and .collect() will convert it into collection
     
-    let config: Config = Config::new(&args);
-    
-    println!("Serching for {}", config.query);
-    println!("In file {}", config.file_path);
+    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}"); // stderr 
+        process::exit(1);
+    });
 
-    let contents: String = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
-    println!("with text:\n\n{contents}");
+    if let Err(e) = minigrep::run(config){
+        eprintln!("Application error: {e}"); // stderr
+        process::exit(1);
+    }
 }
+
